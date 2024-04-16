52cf10e2ef55ab609718b164730dd65d098110e7
phoenix <basilbabyskaria@gmail.com>
2024-04-16 18:41:20 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -11,27 +11,27 @@ function App() {
     }
   }
   function openTabs2() {
+    window.open("https://rewards.bing.com/?signin=1&FORM=ANNRW1", "_blank");
+    if (window.location.href.match("https://www.bing.com/")) {
+      setTimeout(() => {
+        let x = document.querySelectorAll(".mee-icon");
+        if (x.length > 0) {
+          try {
+            for (let j = 0; 1 < x.length; j++) {
+              x[j].click();
+            }
+          } catch {
+            console.log("Done");
+          }
+        }
+      }, 3000);
+    }
     for (let i = 10; i < 41; i++) {
       setTimeout(() => {
         window.open(
           `https://www.bing.com/search?q=${i}&qs=n&form=QBRE&sp=-1&ghc=1&lq=0&pq=3&sc=11-1&sk=&cvid=D087C070AE1041D694CD1F5B18C39721&ghsh=0&ghacc=0&ghpl=`
         );
       }, (i - 10) * 7000);
-      if (i === 40) {
-        window.open("https://rewards.bing.com/?signin=1&FORM=ANNRW1", "_blank");
-        setTimeout(() => {
-          let x = document.querySelectorAll(".mee-icon-AddMedium");
-          if (x.length > 0) {
-            try {
-              for (i = 0; 1 < x.length; i++) {
-                x[i].click();
-              }
-            } catch {
-              console.log("Done");
-            }
-          }
-        }, 7000);
-      }
     }
   }
   return (
